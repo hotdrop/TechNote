95c2d735724a84686d49079e99476fcfee7d06b9
kenji <hotdrop.tkenji@gmail.com>
2023-11-07 16:54:26 +0900

    クラス名をrenameする際にキャメルケースを誤ったので修正

--- lib/ui/info/information_page_controller.dart
+++ lib/ui/info/information_page_controller.dart
@@ -4,7 +4,7 @@ import 'package:package_info_plus/package_info_plus.dart';
 part 'information_page_controller.g.dart';
 
 @riverpod
-class informationPageController extends _$informationPageController {
+class InformationPageController extends _$InformationPageController {
   @override
   Future<PackageInfo> build() async {
     return await PackageInfo.fromPlatform();
